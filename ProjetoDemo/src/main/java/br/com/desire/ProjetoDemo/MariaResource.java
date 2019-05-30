37a9f74b5f98c4500c86de9427ea836b4bd8aaee
Luciano Borges <luc.gaubatz@gmail.com>
2019-05-30 02:29:09 -0300

    Novos métodos em Mariadepository

--- ProjetoDemo/src/main/java/br/com/desire/ProjetoDemo/MariaResource.java
+++ ProjetoDemo/src/main/java/br/com/desire/ProjetoDemo/MariaResource.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -26,27 +28,26 @@ public class MariaResource {
 		array.get(0);	// getByID
 		return "oi";
 	}
-	
+
 	// tipo de operação e de qual forma deve apresentar o resultado
 	@GET
 	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML}) // produzir no formato XML
-	
 	public List<Maria> getMarias() {
 		System.out.println("getMarias Calling....");
-		
+
 		return repo.getMarias();
 	}
-	
+
 	@GET
 	@Path("maria/{id}") //id é passado com parametro no URI do recurso
 	//@Produces(MediaType.APPLICATION_XML) // produzir no formato XML
 	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON}) // produzir no formato XML ou JSON
-		public Maria getMaria(@PathParam("id") int id) { // reconhecer que id é um parametro
+	public Maria getMaria(@PathParam("id") int id) { // reconhecer que id é um parametro
 		System.out.println("getMaria Calling....");
-		
+
 		return repo.getMaria(id);
 	}
-	
+
 	@POST
 	@Path("maria")
 	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
@@ -54,6 +55,31 @@ public class MariaResource {
 		System.out.println(a);;
 		repo.create(a);
 		return a;
-		
+
+	}
+
+	@POST
+	@Path("jogador/edita/{id}") //id é passado com parametro no URI do recurso
+	//@Produces(MediaType.APPLICATION_XML) // produzir no formato XML
+	@Produces(MediaType.TEXT_PLAIN) // produzir no formato XML ou JSON
+	@Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public String edita(Maria a, @PathParam("id") int id) { // reconhecer que id é um parametro
+		String msg= "";
+		System.out.println(a.getID());
+		repo.edit(a,id);
+		msg = "editado com sucesso";
+		return msg;
+	}
+
+	@DELETE
+	@Path("jogador/deleta/{id}") //id é passado com parametro no URI do recurso
+	//@Produces(MediaType.APPLICATION_XML) // produzir no formato XML
+	@Produces(MediaType.TEXT_PLAIN) // produzir no formato XML ou JSON
+	@Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public String delete(Maria a, @PathParam("id") int id) { // reconhecer que id é um parametro
+		String msg= "";
+		repo.deletejogador(id);
+		msg = "removido com sucesso";
+		return msg;
 	}
 }
--- ProjetoDemo/src/main/java/br/com/desire/ProjetoDemo/Mariadepository.java
+++ ProjetoDemo/src/main/java/br/com/desire/ProjetoDemo/Mariadepository.java
@@ -5,42 +5,55 @@ import java.util.List;
 
 public class Mariadepository {
 	List<Maria> marias;
-		public Mariadepository() {
-	marias = new ArrayList<>();	
+	
+	public Mariadepository() {
+		marias = new ArrayList<>();	
 		Maria a1 = new  Maria();
-	a1.setID(101);
-	a1.setNome("Maria");
-	a1.setPontos(10);
+		a1.setID(101);
+		a1.setNome("Maria");
+		a1.setPontos(10);
 		Maria a2 = new  Maria();
-	a2.setID(102);
-	a2.setNome("GIL");
-	a2.setPontos(10);
+		a2.setID(102);
+		a2.setNome("GIL");
+		a2.setPontos(10);
 		Maria a3 = new  Maria();
-	a3.setID(103);
-	a3.setNome("Conte");
-	a3.setPontos(11);
+		a3.setID(103);
+		a3.setNome("Conte");
+		a3.setPontos(11);
 		marias.add(a1);
-	marias.add(a2);
-	marias.add(a3);
+		marias.add(a2);
+		marias.add(a3);
 	}
 	
+	// listar todas as Marias
 	public List<Maria> getMarias(){
-		
 		return marias;
 	}
 	
-public Maria getMaria(int id){
+	// get Maria by ID
+	public Maria getMaria(int id){
 		for(Maria a: marias) {
 			if (a.getID()==id) 
 				return a;
 		}
-		
+
 		return new Maria();
 	}
-
-public void create(Maria a) {
-	// TODO Auto-generated method stub
-	marias.add(a);
-}
 	
+	// criar Maria
+	public void create(Maria a) {
+		// TODO Auto-generated method stub
+		marias.add(a);
+	}
+
+	public void edit(Maria a, int id) {
+		// TODO Auto-generated method stub
+		System.out.println("editar");
+	}
+
+	public void deletejogador(int id) {
+		// TODO Auto-generated method stub
+		System.out.println("apagar");
+	}
+
 }
